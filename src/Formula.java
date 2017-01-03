23e1abd32cec1a95f914aab8071dc74d622b412d
Joshua Zeltser <joshua.zeltser@gmail.com>
2017-01-03 16:23:43 +0000

    Created propositional variable class and formula stub class

---
+++ src/Formula.java
@@ -0,0 +1,7 @@
+/**
+ * Created by joshuazeltser on 03/01/2017.
+ */
+public class Formula {
+
+
+}
---
+++ src/Proposition.java
@@ -0,0 +1,11 @@
+/**
+ * Created by joshuazeltser on 03/01/2017.
+ */
+public class Proposition {
+
+    private String name;
+
+    public Proposition(String name) {
+        this.name = name;
+    }
+}
