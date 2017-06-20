3795407672b41049e27df30d3aca6749ca150a81
Joshua Zeltser <joshua.zeltser@gmail.com>
2017-06-20 22:21:51 +0100

    completed user guide page including user guide and rules

--- src/main/java/model/NDRulesController.java
+++ src/main/java/model/NDRulesController.java
@@ -4,33 +4,28 @@ import org.apache.maven.surefire.shade.org.apache.commons.io.IOUtils;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Controller
-public class UserGuideController {
-
-	    @GetMapping("/userGuide")
-		public void expressionForm(Model model) {
-
-		}
-
-		@PostMapping("/userGuide")
-		public void expressionSubmit() {
-
-		}
+public class NDRulesController {
 
 
-	@RequestMapping(value = "/User-Guide", method = RequestMethod.GET)
+
+
+	@RequestMapping(value = "/ND-Rules", method = RequestMethod.GET)
 	public void getFile(HttpServletResponse response) {
 		try {
 			DefaultResourceLoader loader = new DefaultResourceLoader();
-			InputStream is = loader.getResource("static/userGuide.pdf").getInputStream();
+			InputStream is = loader.getResource("static/NDrules.pdf").getInputStream();
 			IOUtils.copy(is, response.getOutputStream());
-			response.setHeader("Content-Disposition", "attachment; filename=userGuide.pdf");
+			response.setHeader("Content-Disposition", "attachment; filename=NDrules.pdf");
 			response.flushBuffer();
 		} catch (IOException ex) {
 			throw new RuntimeException("IOError writing file to output stream");
